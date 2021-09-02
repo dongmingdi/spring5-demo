a5e590d2347017b7ad81ad4ff8e8607246b8a79c
dongmingdi <176963928@qq.com>
2021-09-02 13:13:08 +0800

    级联赋值

--- src/com/dongmingdi/spring5/test/TestSpringBean2.java
+++ src/com/dongmingdi/spring5/test/TestSpringBean2.java
@@ -30,4 +30,15 @@ public class TestSpringBean2 {
 
         System.out.println(emp);
     }
+
+    @Test
+    public void testTreeBean() {
+        // 1 加载spring配置文件
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
+
+        // 2 获取配置创建的对象
+        Emp emp = context.getBean("emp1", Emp.class);
+
+        System.out.println(emp);
+    }
 }
