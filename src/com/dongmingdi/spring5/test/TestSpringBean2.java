6d88112e545cf34439079de33c391ccd0969f893
dongmingdi <176963928@qq.com>
2021-09-02 13:07:47 +0800

    内部bean

--- src/com/dongmingdi/spring5/test/TestSpringBean2.java
+++ src/com/dongmingdi/spring5/test/TestSpringBean2.java
@@ -1,14 +1,16 @@
 package com.dongmingdi.spring5.test;
 
+import com.dongmingdi.spring5.bean.Emp;
 import com.dongmingdi.spring5.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpringBean2 {
 
     @Test
-    public void testAdd() {
+    public void testOutBean() {
         // 1 加载spring配置文件
         ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
 
@@ -16,5 +18,16 @@ public class TestSpringBean2 {
         UserService userService = context.getBean("userService", UserService.class);
 
         userService.add();
+    }
+
+    @Test
+    public void testInBean() {
+        // 1 加载spring配置文件
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
+
+        // 2 获取配置创建的对象
+        Emp emp = context.getBean("emp", Emp.class);
+
+        System.out.println(emp);
     }
 }
