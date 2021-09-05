65a28fbd7cc65ce655d26bd8781607d7f49ae5ae
dongmingdi <176963928@qq.com>
2021-09-05 17:54:59 +0800

    注解注入属性 @Resource @Value

--- src/com/dongmingdi/spring5/service/UserService.java
+++ src/com/dongmingdi/spring5/service/UserService.java
@@ -3,8 +3,11 @@ package com.dongmingdi.spring5.service;
 import com.dongmingdi.spring5.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 //@Component(value = "userService")
 @Service
@@ -14,14 +17,20 @@ public class UserService {
 //        this.userDao = userDao;
 //    }
 
-    @Autowired
-    @Qualifier(value = "userDaoImpl1")
-    UserDao userDao;
+//    @Autowired
+//    @Qualifier(value = "userDaoImpl1")
+//    UserDao userDao;
+
+    @Resource(name = "userDaoImpl1")
+    private UserDao userDao;
+
+    @Value(value = "abc")
+    private String uname;
 
 
 
     public void add() {
-        System.out.println("service add .....");
+        System.out.println("service add ..... " + uname);
         userDao.update();
     }
 }
