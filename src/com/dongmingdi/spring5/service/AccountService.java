08b5252ff456d593cb87a26a82355810d0a3f5fd
dongmingdi <176963928@qq.com>
2021-09-06 22:58:54 +0800

    Transactional 注解参数

--- src/com/dongmingdi/spring5/service/AccountService.java
+++ src/com/dongmingdi/spring5/service/AccountService.java
@@ -3,8 +3,14 @@ package com.dongmingdi.spring5.service;
 import com.dongmingdi.spring5.dao.AccountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.zip.ZipException;
 
 @Service
+@Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.REPEATABLE_READ, timeout = 5, readOnly = true, rollbackFor = {NullPointerException.class}, noRollbackFor = ZipException.class)
 public class AccountService {
     @Autowired
     public AccountDao accountDao;
@@ -12,8 +18,22 @@ public class AccountService {
     // 转账的方法
     public void accountMoney() {
 
-        accountDao.reduceMoney();
+//        try {
+//            //　开启事务
 
-        accountDao.addMoney();
+            accountDao.reduceMoney();
+
+            int i = 10/0;
+
+            accountDao.addMoney();
+//
+//            // 提交
+//        } catch (Exception e) {
+//            // 回滚
+//        } finally {
+//            // 必执行逻辑
+//        }
+
+
     }
 }
